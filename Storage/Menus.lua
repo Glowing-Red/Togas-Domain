cbbb89cb2e90063b277541e15eb2cfb63954bc77
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-26 15:24:02 +0100

    Add files via upload

---
+++ Storage/Menus.lua
@@ -0,0 +1,15 @@
+Menus = {
+	
+	BGTD = {
+		Name = "Bubble Gum Tower Defense",
+		SearchName = "Bubble Gum Tower Defense",
+		Icon = "Custom",
+		CustomIcon = "6948037100",
+		CustomIconColour = Color3.fromRGB(255, 242, 211),
+		CustomTextColour = Color3.fromRGB(255, 255, 255),
+		BackgroundColour = Color3.fromRGB(68, 131, 49),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua",
+	},
+}
+
+return Menus
